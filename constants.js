0ac7297525e5aa9b366071aab646b020d67a28ba
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2025-02-25 19:46:58 +0600

    Add help manual text.

--- constants.js
+++ constants.js
@@ -3,15 +3,32 @@ const ONE_MINUTE = ONE_SECOND * 60;
 const ONE_HOUR = ONE_MINUTE * 60;
 const ONE_DAY = ONE_HOUR * 24;
 
+const HELP_ARG = "--help";
+
 const START_DATE_ARG_TITLE = "--startDate=";
 const END_DATE_ARG_TITLE = "--endDate=";
 
-const DATE_INPUT_FORMAT_TEXT = "Date Input Format: YYYY-MM-DD";
+const DATE_INPUT_FORMAT_TEXT = "Input Date Format: YYYY-MM-DD";
 
-const MANUAL_TEXT = `MANUAL TEXT HERE....`
+const MANUAL_TEXT = `Description:
+    Find the n-th day starting from a specific date.
+
+${DATE_INPUT_FORMAT_TEXT}
+
+For Command-Line Input:
+    - Run 'npm start -- --startDate=[YYYY-MM-DD] --endDate=[YYYY-MM-DD]'.
+        Example: 'npm start -- --startDate=2000-01-30 --endDate=2000-02-10'.
+    - '--startDate=[YYYY-MM-DD]' is required.
+    - '--endDate=[YYYY-MM-DD]' is optional. Default is today's date.
+For Console Input:
+    - Run 'npm start'
+For Help Manual:
+    - Run 'npm start -- --help'
+`;
 
 module.exports = {
     ONE_DAY,
+    HELP_ARG,
     START_DATE_ARG_TITLE,
     END_DATE_ARG_TITLE,
     DATE_INPUT_FORMAT_TEXT,
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const processCmdLineArgs = require("./processCmdLineArgs");
 const processConsoleInput = require("./processConsoleInput");
+const { printManualAndExit } = require("./helpers");
+const { HELP_ARG } = require("./constants");
 
 const cmdLineArgs = process.argv.slice(2, 4);
 
 const main = async () => {
     let result;
+
+    if (cmdLineArgs.includes(HELP_ARG)) {
+        printManualAndExit();
+    }
 
     if (cmdLineArgs.length > 0) {
         result = processCmdLineArgs(cmdLineArgs);
